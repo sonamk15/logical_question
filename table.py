37803467b5a6e4f925582e90331a560432498b61
sonamk15 <sonamk18@navgurukul.org>
2019-06-08 16:45:15 +0530

    initial commit

---
+++ table.py
@@ -0,0 +1,20 @@
+
+# tab=[2,5]
+# i=0
+# while i<len(tab)-1:
+#     j=1
+#     while j<=10:
+#         print tab[i]*j," \t", tab[i+1]*j
+#         j+=1
+#     i+=1
+
+
+            
+tab=[2,5,6]
+j=1
+while j<=10:
+    print tab[0]*j," \t", tab[1]*j,"\t",tab[2]*j
+    j+=1
+
+
+
